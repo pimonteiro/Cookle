48b9cc48615395b40479ff38bae223ca23c1d052
Eduardo Barbosa <herulume@protonmail.com>
2019-05-01 15:49:27 +0000

    m

--- src/Cookle/Startup.cs
+++ src/Cookle/Startup.cs
@@ -33,9 +33,10 @@ namespace Cookle
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+           
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<UserContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("UserContext")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
