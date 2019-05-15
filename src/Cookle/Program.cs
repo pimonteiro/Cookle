e4692596ade765d7b3723f3e06107c7d66d80fbb
brunomartins <brunocmartins@live.com.pt>
2019-05-15 17:02:13 +0100

    I think this is windows only

--- src/Cookle/Program.cs
+++ src/Cookle/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Cookle.Utils;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,6 +15,7 @@ namespace Cookle
     {
         public static void Main(string[] args)
         {
+            Voice.Speak("Hello darkness my old friend");
             CreateWebHostBuilder(args).Build().Run();
         }
 
--- src/Cookle/Utils/Voice.cs
+++ src/Cookle/Utils/Voice.cs
@@ -12,7 +12,7 @@ namespace Cookle.Utils
             // Creates an instance of a speech config with specified subscription key and service region.
             // Replace with your own subscription key and service region (e.g., "westus").
             // The default language is "en-us".
-            var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+            var config = SpeechConfig.FromSubscription("878cd19d351d40639f7164f64ddfdd5b", "westeurope");
 
             // Creates a speech synthesizer using the default speaker as audio output.
             using (var synthesizer = new SpeechSynthesizer(config))
@@ -39,41 +39,47 @@ namespace Cookle.Utils
             }
         }
 
-        private static async Task TranslationContinuousRecognitionAsync(string output)
+        public static async Task RecognizeSpeechAsync(string output)
         {
-            // Creates an instance of a speech translation config with specified subscription key and service region.
-            // Replace with your own subscription key and service region (e.g., "westus").
-            var config = SpeechTranslationConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key // and service region (e.g., "westus").
+            var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
 
-            // Sets source and target languages.
-            string fromLanguage = "en-US";
-            config.SpeechRecognitionLanguage = fromLanguage;
-            config.AddTargetLanguage("de");
-
-            // Creates a translation recognizer using microphone as audio input.
-            using (var recognizer = new TranslationRecognizer(config))
+            // Creates a speech recognizer.
+            using (var recognizer = new SpeechRecognizer(config))
             {
-                // Subscribes to events.
-                recognizer.Recognizing += (s, e) =>
+                // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+                // single utterance is determined by listening for silence at the end or until a maximum of 15
+                // seconds of audio is processed.  The task returns the recognition text as result. 
+                // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+                // shot recognition like command or query. 
+                // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+                var result = await recognizer.RecognizeOnceAsync();
+
+                // Checks result.
+                if (result.Reason == ResultReason.RecognizedSpeech)
                 {
-                    Console.WriteLine($"RECOGNIZING in '{fromLanguage}': Text={e.Result.Text}");
-                    output = e.Result.Reason.ToString();
-                };
-
-                recognizer.Canceled += (s, e) => { output = "Canceled"; };
-
-                // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
-                await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
-
-                do
+                    output = result.Text;
+                }
+                else if (result.Reason == ResultReason.NoMatch)
                 {
-                    Console.WriteLine("Press Enter to stop");
-                } while (Console.ReadKey().Key != ConsoleKey.Enter);
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
 
-                // Stops continuous recognition.
-                await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
             }
         }
+            
 
         public static void Speak(string text)
         {
@@ -83,7 +89,7 @@ namespace Cookle.Utils
         public static string Listen()
         {
             string output = "Canceled";
-            TranslationContinuousRecognitionAsync(output).Wait();
+            RecognizeSpeechAsync(output).Wait();
             return output;
         }
     }
