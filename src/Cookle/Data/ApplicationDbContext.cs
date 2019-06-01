11541bdb07d72bfde79a31d5f7714ede8eb7bc42
Eduardo Barbosa <herulume@protonmail.com>
2019-06-01 21:47:31 +0100

    m

--- src/Cookle/Data/ApplicationDbContext.cs
+++ src/Cookle/Data/ApplicationDbContext.cs
@@ -14,32 +14,110 @@ namespace Cookle.Data
             : base(options)
         {
         }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<Historico>().HasKey(table => new {
+            builder.Entity<Historico>().HasKey(table => new
+            {
                 table.UserId, table.ReceitaId
             });
-            builder.Entity<Frigorifico>().HasKey(table => new {
+            builder.Entity<Frigorifico>().HasKey(table => new
+            {
                 table.UserId, table.IngredienteId
             });
-            builder.Entity<IngredienteReceita>().HasKey(table => new {
+            builder.Entity<IngredienteReceita>().HasKey(table => new
+            {
                 table.IngredienteId, table.ReceitaId
             });
-            builder.Entity<NutrienteReceita>().HasKey(table => new {
+            builder.Entity<NutrienteReceita>().HasKey(table => new
+            {
                 table.NutrienteId, table.ReceitaId
             });
-            builder.Entity<Passo>().HasKey(table => new {
+            builder.Entity<Passo>().HasKey(table => new
+            {
                 table.Numero, table.ReceitaId
             });
-            builder.Entity<Plano>().HasKey(table => new {
+            builder.Entity<Plano>().HasKey(table => new
+            {
                 table.UserId, table.ReceitaId
             });
-            builder.Entity<PreferenciaIngrediente>().HasKey(table => new {
+            builder.Entity<PreferenciaIngrediente>().HasKey(table => new
+            {
                 table.UserId, table.IngredienteId
             });
+
+
+            builder.Entity<Admin>().HasData(
+                new Admin
+                {
+                    Id = 1,
+                    Email = "admin@admin.com",
+                    Password = "admin"
+                }
+            );
+
+            builder.Entity<Nutriente>().HasData(
+                new Nutriente
+                {
+                    Id = 1,
+                    Nome = "Hidratos de carbono",
+                    Unidade = 0
+                });
+            
+            builder.Entity<Nutriente>().HasData(
+                new Nutriente
+                {
+                    Id = 2,
+                    Nome = "Lípidos",
+                    Unidade = 0
+                });
+            
+            builder.Entity<Nutriente>().HasData(
+                new Nutriente
+                {
+                    Id = 3,
+                    Nome = "Proteínas",
+                    Unidade = 0
+                });
+            
+            builder.Entity<Ingrediente>().HasData(
+                new Ingrediente
+                {
+                    Id = 1,
+                    Nome = "Ovos",
+                });
+            
+            builder.Entity<Ingrediente>().HasData(
+                new Ingrediente
+                {
+                    Id = 2,
+                    Nome = "Farinha de trigo",
+                });
+            
+            builder.Entity<Ingrediente>().HasData(
+                new Ingrediente
+                {
+                    Id = 3,
+                    Nome = "Leite",
+                });
+            
+            builder.Entity<Ingrediente>().HasData(
+                new Ingrediente
+                {
+                    Id = 4,
+                    Nome = "Sal",
+                });
+            
+            builder.Entity<Ingrediente>().HasData(
+                new Ingrediente
+                {
+                    Id = 5,
+                    Nome = "Oleo vegetal",
+                });
         }
+
         public DbSet<Cookle.Models.Admin> Admin { get; set; }
         public DbSet<Cookle.Models.Frigorifico> Frigorifico { get; set; }
         public DbSet<Cookle.Models.Historico> Historico { get; set; }
