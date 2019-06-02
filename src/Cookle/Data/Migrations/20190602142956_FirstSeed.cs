1f88dd0994201b0b0a407fb8f45c5c2bb8339450
Catarina Sousa <ana_96_sousa@hotmail.com>
2019-06-02 15:33:52 +0100

    Data seeding (#12)

---
+++ src/Cookle/Data/Migrations/20190602142956_FirstSeed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cookle.Migrations
+{
+    public partial class FirstSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
