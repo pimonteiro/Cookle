ec4667af7265e53497a886cc302f45b0b0a86e1a
Filipe Monteiro <pimonteiro@protonmail.com>
2019-06-03 15:54:57 +0100

    search fix and home database access

--- src/Cookle/Controllers/ReceitaController.cs
+++ src/Cookle/Controllers/ReceitaController.cs
@@ -227,15 +227,42 @@ namespace Cookle.Controllers
         }
 
         // GET: Receita/Search
-        public async Task<IActionResult> Search(string search)
+        public async Task<IActionResult> Search(string search, int id, string tipo)
         {
             if (!String.IsNullOrEmpty(search))
             {
                 var receitas = await _context.Receita.Where(r => r.Nome.Contains(search)).ToListAsync();
-                return View(receitas);
+
+                if (tipo.Equals("NAN"))
+                {
+                    return View(receitas);
+                }
+                List<IngredienteReceita> ingredientesRe = new List<IngredienteReceita>();
+                List<Ingrediente> ingredientes = new List<Ingrediente>();
+                List<Receita> recRet = new List<Receita>();
+                foreach (var rec in receitas)
+                {
+                    var flag = 0;
+                    ingredientesRe = _context.IngredienteReceita.Where(r => r.ReceitaId == rec.Id).ToList();
+                    foreach (var ingreR in ingredientesRe)
+                    {
+                        ingredientes = _context.Ingrediente.Where(i => i.Id == ingreR.IngredienteId).ToList();
+                    }
+                    foreach (var ing in ingredientes)
+                    {
+                        if (!_context.PreferenciaIngrediente.First(p => p.UserId == id && p.IngredienteId == ing.Id).Tipo.ToString().Equals(tipo))
+                        {
+                            flag++;
+                        }
+                    }
+                    if(flag == 0)
+                        recRet.Add(_context.Receita.First(r => r.Id == rec.Id));
+                }
+               
+                return View(recRet);
             }
-            else
-                return View(_context.Receita.ToList());
+ 
+            return View(_context.Receita.ToList());
         }
     }
 }
